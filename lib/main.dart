1be11a4c35be54c621ffa09eb7ad5c7cbb75c72f
MuhammadSetyaBudiPratama <msetyapratama27@gmail.com>
2022-03-08 02:39:33 +0700

    Pretest Kalkulator

---
+++ lib/main.dart
@@ -0,0 +1,133 @@
+// ignore_for_file: unnecessary_new, deprecated_member_use
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    const appTitle = 'Pretest Kalkulator';
+
+    return MaterialApp(
+      title: appTitle,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(appTitle),
+        ),
+        body: const AddTwoNumbers(),
+      ),
+    );
+  }
+}
+
+class AddTwoNumbers extends StatefulWidget {
+  const AddTwoNumbers({Key? key}) : super(key: key);
+
+  @override
+  _AddTwoNumbersState createState() => _AddTwoNumbersState();
+}
+
+class _AddTwoNumbersState extends State<AddTwoNumbers> {
+  TextEditingController num1controller = TextEditingController();
+  TextEditingController num2controller = TextEditingController();
+  String resulttext = "0";
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            const Text(
+              "Hasil Perhitungan : ",
+              style: TextStyle(
+                fontSize: 25,
+              ),
+            ),
+            Text(
+              resulttext,
+              style: const TextStyle(
+                fontSize: 25,
+              ),
+            ),
+          ],
+        ),
+        Row(
+          children: <Widget>[
+            const Text("Input Nilai Pertama   : "),
+            new Flexible(
+              child: new TextField(
+                keyboardType: TextInputType.number,
+                controller: num1controller,
+              ),
+            ),
+          ],
+        ),
+        Row(
+          children: <Widget>[
+            const Text("Input Nilai Kedua     : "),
+            new Flexible(
+              child: new TextField(
+                keyboardType: TextInputType.number,
+                controller: num2controller,
+              ),
+            ),
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            RaisedButton(
+              child: const Text("+"),
+              onPressed: () {
+                setState(() {
+                  int result = int.parse(num1controller.text) +
+                      int.parse(num2controller.text);
+                  resulttext = result.toString();
+                });
+              },
+            ),
+            RaisedButton(
+              child: const Text("-"),
+              onPressed: () {
+                setState(() {
+                  int result = int.parse(num1controller.text) -
+                      int.parse(num2controller.text);
+                  resulttext = result.toString();
+                });
+              },
+            ),
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            RaisedButton(
+              child: const Text("x"),
+              onPressed: () {
+                setState(() {
+                  int result = int.parse(num1controller.text) *
+                      int.parse(num2controller.text);
+                  resulttext = result.toString();
+                });
+              },
+            ),
+            RaisedButton(
+              child: const Text("/"),
+              onPressed: () {
+                setState(() {
+                  double result = double.parse(num1controller.text) /
+                      double.parse(num2controller.text);
+                  resulttext = result.toStringAsPrecision(3);
+                });
+              },
+            )
+          ],
+        ),
+      ],
+    );
+  }
+}
